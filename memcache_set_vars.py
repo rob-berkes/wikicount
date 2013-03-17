6ad1ac39d9c0acc2c02508a55a8a52ac2bcb82d3
Rob Berkes <rob.berkes@gmail.com>
2013-03-17 00:52:57 +0000

    moved archive to separate memcache script, modified graph for better top 10 display, offsetting zero

--- memcache_set_vars.py
+++ memcache_set_vars.py
@@ -249,26 +249,6 @@ for item in QUERY:
 mc.set('DEBUTS_ARTICLES',send_list,60*60)
 
 
-print 'current month archive...'
-send_list=[]
-#RESULTSET=db.command({'distinct':thCN,'key':'d','query':{'m':int(MONTH),'y':int(YEAR)}})
-RESULTSET=db.command({'distinct':thCN,'key':'d'})
-for d in RESULTSET['values']:
-	rec={'d':d,'m':MONTH,'y':YEAR,'stry':str(YEAR),'strm':str(MONTH),'strd':str(d)}
-	QUERY={'d':int(d),'m':int(MONTH),'y':int(YEAR)}
-	DAYKEY='toplist'+str(YEAR)+str(MONTH)+str(DAY)
-	page_list=[]
-        PAGERESULTSET=db[thCN].find(QUERY).sort('place',1).limit(100)
-	syslog.syslog('memcache-monthly: '+str(DAYKEY)+' '+str(QUERY)+' count: '+str(PAGERESULTSET.count()))
-        for row in PAGERESULTSET:
-		title, utitle=FormatName(row['title'])
-                prec={'place':row['place'],'Hits':row['Hits'],'title':title ,'id':str(row['id']),'linktitle':utitle}
-		GenInfoPage(row['id'])
-                page_list.append(prec)
-                mc.set('DAYKEY',page_list,60*60*24*14)
-        send_list.append(rec)
-mc.set('mcdpDaysList'+str(MONTH)+str(YEAR),send_list,60*60*24)
-
 #print 'on to dec 2012 archives...'
 #send_list=[]
 #RESULTSET=db.command({'distinct':thCN,'key':'d','query':{'m':12}})
